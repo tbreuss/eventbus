a6f0a163f2cf81409378f46bcee8323126830aac
Aaron Ebnöther <aaron.ebnoether@gmail.com>
2018-10-06 13:20:51 +0200

    Add source code documentation for eventbus.js

--- src/eventbus.js
+++ src/eventbus.js
@@ -4,51 +4,78 @@ class EventBus {
         this.events = {};
     }
 
+    /**
+     * Adds listener to EventBus
+     * @param {string} type The name of the event to listen for
+     * @param {function} callback Callback to call when event was triggered
+     * @param {object} scope The scope in which the callback shall be executed
+     * @param  {...any} args Any number of args to be passed to the callback
+     */
     on(type, callback, scope, ...args) {
-        if (typeof this.events[type] === "undefined") {
-            this.events[type] = [];
+        if (typeof this.events[type] === "undefined") { // Check if there is already event of this type registered
+            this.events[type] = []; // If not, create array for it
         }
-        this.events[type].push({scope, callback, args});
+        this.events[type].push({scope, callback, args}); // Finally push new event to events array
     }
 
+    /**
+     * Removes listener from EventBus
+     * @param {string} type The name of the event to remove
+     * @param {function} callback Callback of the event to remove
+     * @param {object} scope The scope of the to be removed event
+     */
     off(type, callback, scope) {
-        if (typeof this.events[type] === "undefined") {
-            return;
+        if (typeof this.events[type] === "undefined") { // Check if event of this type exists
+            return; // If not just return
         }
 
-        let newArray = [];
-        for (const event of this.events[type]) {
+        let newArray = []; // Array that will contain all events except the one removed
+        for (const event of this.events[type]) { // Iterate all events of the specified type
+            // Check if the event of the current iteration and the passed one are equal
             const isSame = event.scope == scope && event.callback == callback;
-            if (!isSame) {
-                newArray.push(event);
+            if (!isSame) { // If they are not the same...
+                newArray.push(event); // ...push the current event to the newArray
             }
         }
 
-        this.events[type] = newArray;
+        this.events[type] = newArray; // Reset the events array with all events except the removed one
     }
 
+    /**
+     * Checks if the passed event is registered in the EventBus
+     * @param {string} type Type of the to be checked event
+     * @param {callback} callback Callback of the to be checked event
+     * @param {object} scope Scope of the to be checked event
+     */
     has(type, callback, scope) {
-        if (typeof this.events[type] === "undefined") {
-            return false;
+        if (typeof this.events[type] === "undefined") { // Check if the passed type even exists
+            return false; // If not, quit method
         }
 
+        // If callback and scope are undefined then every registered event is match, thus any event of the type matches
         let numOfCallbacks = this.events[type].length;
-        if (callback === undefined && scope === undefined) {
-            return numOfCallbacks > 0;
+        if (callback === undefined && scope === undefined) { // If scope and callback are not defined
+            return numOfCallbacks > 0; // If there are any callbacks we can be sure it matches the passed one
         }
 
-        for (const event of this.events[type]) {
-            const scopeIsSame = scope ? event.scope == scope : true;
-            const callbackIsSame = event.callback == callback;
-            return scopeIsSame && callbackIsSame;
+        for (const event of this.events[type]) { // Iterate all events of the passed type
+            const scopeIsSame = scope ? event.scope == scope : true; // Check if scope is equal to the one passed
+            const callbackIsSame = event.callback == callback; // Check if callback is equal to the one passed
+            return scopeIsSame && callbackIsSame; // Check if current event and passed event are equal
         }
 
-        return false;
+        return false; // If there aren't any events it cannot match
     }
 
+    /**
+     * Emits an event on the EventBus
+     * @param {string} type Type of event to emit
+     * @param {object} target The caller 
+     * @param {...any} args Any number of args to be passed to the callback
+     */
     emit(type, target, ...args) {
-        if (typeof this.events[type] === "undefined") {
-            return;
+        if (typeof this.events[type] === "undefined") { // Check if any event of the passed type exists
+            return; // If not, quit method
         }
 
         let bag = {
@@ -56,13 +83,13 @@ class EventBus {
             target: target
         };
         
-        args = [bag].concat(args);
-        const events = this.events[type].slice();
+        args = [bag].concat(args); // Merge arrays
+        const events = this.events[type].slice(); // Little hack to clone array
 
-        for (const event of events) {
-            if (event && event.callback) {
-                const concatArgs = args.concat(event.args);
-                event.callback.apply(event.scope, concatArgs);
+        for (const event of events) { // Iterate all events
+            if (event && event.callback) { // Check if callback of event is set 
+                const concatArgs = args.concat(event.args); // Add args that were passed
+                event.callback.apply(event.scope, concatArgs); // Call callback
             }
         }
     }
